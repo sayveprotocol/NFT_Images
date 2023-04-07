fa4f5aafb8af15f90ec3b2970a79162c0f5a0063
sayve <lets@sayve.money>
2023-04-07 10:06:33 +0700

    removed .DS

---
+++ ds_remover.sh
@@ -0,0 +1,10 @@
+find . -name ".*" -delete
+rm examples/.DS_Store
+rm examples/layers/.DS_Store
+rm examples/layers/Body/.DS_Store
+rm examples/layers/Bonus_item/.DS_Store
+rm examples/layers/Character/.DS_Store
+rm examples/layers/Hands/.DS_Store
+rm examples/layers/Shoes/.DS_Store
+rm examples/layers/Handheld/.DS_Store
+rm examples/layers/Headwear/.DS_Store
